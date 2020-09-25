ed26b546daf422455c161b41eec83fcef0fb2161
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-09-26 00:22:23 +0900

    feat: create simple sample

--- sample/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ sample/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -1,11 +1,23 @@
 package jp.kaleidot725.sample
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import jp.kaleidot725.slideshow.SlideFactory
+import kotlinx.android.synthetic.main.activity_main.*
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+
+        slide_show_view.apply {
+            setSlides(
+                arrayListOf(
+                    SlideFactory.create(R.drawable.ic_launcher_background),
+                    SlideFactory.create(R.drawable.ic_launcher_foreground)
+                )
+            )
+            setInterval(5000)
+            start()
+        }
     }
 }
