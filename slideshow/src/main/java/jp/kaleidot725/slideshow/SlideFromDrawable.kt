37b880eb27713d22a55658a074dd4555e8f8af09
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-09-25 23:04:01 +0900

    feat: impl SlideFromDrawable

--- slideshow/src/main/java/jp/kaleidot725/slideshow/SlideFromDrawable.kt
+++ slideshow/src/main/java/jp/kaleidot725/slideshow/SlideFromDrawable.kt
@@ -3,8 +3,8 @@ package jp.kaleidot725.slideshow
 import android.graphics.drawable.Drawable
 import android.widget.ImageSwitcher
 
-internal class SlideFromDrawable(val drawable: Drawable) : Slide {
+internal class SlideFromDrawable(private val drawable: Drawable) : Slide {
     override fun load(imageSwitcher: ImageSwitcher) {
-        TODO("Not yet implemented")
+        imageSwitcher.setImageDrawable(drawable)
     }
 }
