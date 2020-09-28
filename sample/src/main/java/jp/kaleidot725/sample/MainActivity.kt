52fbc9a6278aee7f03df23f105ebf29aa2f9c47e
Kaleidot725 <hakodate.katsuragawa.yusuke@gmail.com>
2020-09-28 21:08:41 +0900

    feat: replace sample image

--- sample/src/main/java/jp/kaleidot725/sample/MainActivity.kt
+++ sample/src/main/java/jp/kaleidot725/sample/MainActivity.kt
@@ -2,6 +2,7 @@ package jp.kaleidot725.sample
 
 import android.os.Bundle
 import android.view.animation.AlphaAnimation
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import jp.kaleidot725.slideshow.SlideFactory
 import kotlinx.android.synthetic.main.activity_main.*
@@ -10,6 +11,7 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         slide_show_view.slides = DEFAULT_SLIDES
+        slide_show_view.scaleType = DEFAULT_SCALE_TYPE
         slide_show_view.interval = DEFAULT_INTERVAL
         slide_show_view.inAnimation = DEFAULT_IN_ANIMATION
         slide_show_view.outAnimation = DEFAULT_OUT_ANIMATION
@@ -17,11 +19,15 @@ class MainActivity : AppCompatActivity(R.layout.activity_main) {
     }
 
     companion object {
+        private const val DEFAULT_INTERVAL = 2000L
         private val DEFAULT_SLIDES = arrayListOf(
-            SlideFactory.create(R.drawable.ic_launcher_background),
-            SlideFactory.create(R.drawable.ic_launcher_foreground)
+            SlideFactory.create(R.drawable.one),
+            SlideFactory.create(R.drawable.two),
+            SlideFactory.create(R.drawable.three),
+            SlideFactory.create(R.drawable.four),
+            SlideFactory.create(R.drawable.five)
         )
-        private const val DEFAULT_INTERVAL = 1000L
+        private val DEFAULT_SCALE_TYPE = ImageView.ScaleType.CENTER
         private val DEFAULT_IN_ANIMATION = AlphaAnimation(0.0f, 1.0f).apply { duration = 1000 }
         private val DEFAULT_OUT_ANIMATION = AlphaAnimation(1.0f, 0.0f).apply { duration = 1000 }
     }
